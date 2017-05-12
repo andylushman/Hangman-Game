193fad1523d29cb30ae46afd867476300b07e889
Andy Lushman <andylushman@gmail.com>
2017-05-12 13:40:21 -0600

    Added comments to JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,23 @@
-//Choosing a Random Word
-/*var words = ["enduro", "downhill", "travel", "helment", "brem", "hardtail", "shred", "bonk", "clipless", "cyclocross", "doubletrack", "singletrack", "dulie", "ratchet", "snakebite", "switchback", "trackstand", "schrader", "prista" ];
+//Word Bank
+var words = ["enduro", "downhill", "travel", "helment", "brem", "hardtail", "shred", "bonk", "clipless", "cyclocross", "doubletrack", "singletrack", "dulie", "ratchet", "snakebite", "switchback", "trackstand", "schrader", "prista" ];
 
+//Chosing a random word
 var word = words[Math.floor(Math.random() * words.length)];
 
+//Array for answers
 var answerArray = [];
 
+//Create _ for each random letter in the word
 for(var i = 0; i < word.length; i++){
   answerArray[i]= "_";
 }
 
+//How many letters left
 var remainingLetters = word.length
 
 
+//Game Loop
 while (remainingLetters > 0) {
-
-  // Game code goes here
 
   // Show the player their progress
   alert(answerArray.join(" "));
