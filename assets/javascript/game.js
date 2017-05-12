b7ffeea4d0a81d7bebff43ca422841b86d8dec57
Andy Lushman <andylushman@gmail.com>
2017-05-12 14:41:14 -0600

    Moved var html into function onkeyup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,28 +16,16 @@ for(var i = 0; i < word.length; i++){
 
 var blankWord = answerArray.join(" ");
 
-  //How many letters left
-var remainingLetters = word.length
-
-//Insert content into index.html
-var html = "<p>Press any key to get started!</p>" +
-            "<p> Wins: " wins +
-            "<p>Current Word</p>" + "<br>" +
-            blankWord;
-            "<p>Numpber of Guesses Remaining</p>" + "<br>" +
-            remainingLetters;
-            "<p>Letters Already Guessed</p>";
-
-document.querySelector("#game-interface").innerHTML =  html;
-
-
-//Game Loop
-while (remainingGuesses > 0) {
+  //Letters already Guessed
+var lettersGuessed =
 
   // Take input from the player
-    document.onkeyup = function(event) {
-      // Determines which key was pressed
-      var userGuess = event.key;
+document.onkeyup = function(event) {
+  // Determines which key was pressed
+  var userGuess = event.key;
+
+  //Game Loop
+  while (remainingGuesses > 0) {
 
     if(userGuess === null){
       break;
@@ -53,5 +41,14 @@ while (remainingGuesses > 0) {
         }
       } wins ++;
     }//End For Loop
-  }//End function
-}//End While loop
+  }//End While Loop
+  var html = "<p>Press any key to get started!</p>" +
+            "<p> Wins: " + wins + "</p>" +
+            "<p>Current Word</p>" + "<br>" +
+            blankWord;
+            "<p>Numpber of Guesses Remaining</p>" + "<br>" +
+            remainingLetters;
+            "<p>Letters Already Guessed</p>";
+
+  document.querySelector("#game-interface").innerHTML =  html;
+}//End Function
